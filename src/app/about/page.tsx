c6f47268f124f9812a4b834fe4a9adacf4a4614a
0xRenegade <therenegadecoding@gmail.com>
2025-02-21 11:55:38 -0600

    grammer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <main className="flex flex-1 flex-col items-center justify-center text-center p-4 gap-5">
         <img src="/next.svg" alt="Nextjs Logo" className="w-md" />
         <div className="text-slate-950 w-100">
-          Nextjs Commerce is a mock ecommerce website created for educational
+          Nextjs Commerce is a mock eCommerce website created for educational
           purposes and portfolio building. This is not intended to take actual
           payments or sell real products. If you would like to use this code to
           make your own eCommerce website, feel free to do so.
