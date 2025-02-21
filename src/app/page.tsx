f17fef2105bb25d3b62925879c275ce10d48a22f
0xRenegade <therenegadecoding@gmail.com>
2025-02-21 11:39:50 -0600

    move navbar and footer to site wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import NavBar from "@/components/layout/NavBar"
-import Footer from "@/components/layout/Footer"
 import SiteWrapper from "@/components/layout/SiteWrapper"
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 export default function Home() {
   return (
     <SiteWrapper>
-      <NavBar />
       <main className="flex flex-1 flex-col items-center justify-center text-center p-4 gap-5">
         <img src="/next.svg" alt="Nextjs Logo" className="w-md" />
         <div className="italic text-6xl text-slate-950">eCommerce!</div>
@@ -18,7 +14,6 @@ export default function Home() {
           Products
         </Link>
       </main>
-      <Footer />
     </SiteWrapper>
   )
 }
