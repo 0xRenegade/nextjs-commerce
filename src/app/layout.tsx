d5414db4fdfbde30981aab9fc83b738ff9ec3f86
0xRenegade <therenegadecoding@gmail.com>
2025-03-14 09:29:03 -0500

    add back meta data info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-"use client"
+// "use client"
 
-// import type { Metadata } from "next"
+import type { Metadata } from "next"
 import "./globals.css"
 import NavBar from "@/components/layout/NavBar"
 import Footer from "@/components/layout/Footer"
 
-// export const metadata: Metadata = {
-//   title: "Nextjs Commerce",
-//   description: "Created by 0xRenegade",
-// }
+export const metadata: Metadata = {
+  title: "Nextjs Commerce",
+  description: "Created by 0xRenegade",
+}
 
 export default function RootLayout({
   children,
